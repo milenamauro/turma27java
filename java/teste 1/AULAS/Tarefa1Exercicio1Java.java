58d3e97711168657cdd75ca24a0441ac3d91b7bb
milenamauro <milena.93@hotmail.com>
2021-07-01 22:10:15 -0300

    Seii la mas vamo la

---
+++ java/teste 1/AULAS/Tarefa1Exercicio1Java.java
@@ -0,0 +1,27 @@
+
+public class Tarefa1Exercicio1Java {
+
+	//Faça um sistema que leia a idade de uma pessoa expressa em anos, 
+	//meses e dias e mostre-a expressa apenas em dias. 
+public class programa{
+	public static void main(string[]args) {
+		
+		int anos; meses; dias; idadeEmdias
+		
+		System.out.println ("Entre com anos")
+		anos= entrada.nextInt();
+		
+		
+		System.out.println ("Entre com meses")
+		meses= entrada.nextInt();
+		
+		
+		System.out.println ("Entre com dias")
+		IdadeDias = entrada.nextInt();
+		
+		int idadeEmdias = anos*365 + meses*30 + dias*1
+	   System.out.print("Idade total em dias = "+IdadeTotalDias+"\n");
+		
+	}
+}
+}
---
+++ java/teste 1/Java exercicios/src/Tarefa1Exercicio1Java.java
@@ -0,0 +1,4 @@
+
+public class Tarefa1Exercicio1Java {
+
+}
