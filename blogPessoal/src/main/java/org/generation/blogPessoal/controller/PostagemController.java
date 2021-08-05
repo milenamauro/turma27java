be2e6868e45c20d92a26c31e4686876b486a0bc2
milenamauro <milena.93@hotmail.com>
2021-08-05 19:52:21 -0300

    Blogpessoal

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -1,11 +1,11 @@
 package org.generation.blogPessoal.controller;
 
-import java.awt.List;
-import org.generation.blogPessoal.repository.PostagemRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,11 +19,10 @@ import org.generation.blogPessoal.repository.PostagemRepository;
 
 //mostrar que é um controler
 @RestController
-//uri consume essa classe,  
+ 
 @RequestMapping ("/postagens")
-//o front end vem de uma origem diferente e consume essa parte
-@CrossOrigin ("*")
-public class PostagemController<Postagem> {
+ @CrossOrigin ("*")
+public class PostagemController  {
     
 	//spting, todos serviçoes dessa interface esteja acessada a partir do controller
 	@Autowired
@@ -34,20 +33,32 @@ public class PostagemController<Postagem> {
 	public ResponseEntity<List<Postagem>> GetAll(){
 	return ResponseEntity.ok(repository.findAll());
 	}
+	
 	@GetMapping("/{id}")
-	public ResponseEntity<org.generation.blogPessoal.model.Postagem> GetById (@PathVariable long id){
+	public ResponseEntity<Postagem> GetById (@PathVariable long id){
 		return repository.findById(id)
 				.map(resp -> ResponseEntity.ok(resp)) 
 				.orElse(ResponseEntity.notFound().build());
-		
-	}
+		}
+	
 	 @GetMapping("/titulo/{titulo}")
 	 public ResponseEntity<List <Postagem>> GetByTitulo(@PathVariable String titulo){
-		 return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+			 return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
+		 }
+	 @PostMapping
+	 public ResponseEntity<Postagem> post (@RequestBody Postagem postagem) {
+		 return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+		 
 	 }
+	 
 	 @PutMapping
-	 public ResponseEntity<org.generation.blogPessoal.model.Postagem> put (@RequestBody Postagem) {
+	 public ResponseEntity<Postagem> put (@RequestBody Postagem postagem) {
 		 return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
 		 
+	 }
+	 
+	 @DeleteMapping ("/{id}")
+	 public void delete (@PathVariable long id) {
+		 repository.deleteById(id);
 	 }
 }
--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,39 +6,45 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size; 
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //essa anotação indica essa classe é uma entidade do jpa ibernate
 @Entity
 //essa entidade dentro do banco de dados ela vira uma tabela, sera de postagens
-@Table (name="postagem") //crea postagem
+@Table(name = "postagem") // crea postagem
 //primeiro um ID
 public class Postagem {
-	 
-	 
-	 @Id //Tudo isso até idenitty se comporta como chave primaria
-	@GeneratedValue (strategy = GenerationType.IDENTITY) 	//um valor que será gerado auto incremento
-   private long id; //big int
-		  
-	@NotNull //Anotacao para dizer que não pode ter titulo nulo
-	@Size (min= 5, max=100) // anotação para a quantidade de caracter que o cliente consegue mandar como titulo
-	private String titulo; //tipo o varchar (100)
-	
 
-		@NotNull //Anotacao para dizer que não pode ter titulo nulo
-		// anotação para a quantidade de caracter que o cliente consegue mandar como titulo
-		@Size (min= 10, max=500)
-	    private String texto;
-		
-		 
-		@Temporal (TemporalType.TIMESTAMP) //anotação que vai indicar o JPA ibernate que estamos trabalhando com tempo
-		private Date date =new java.sql.Date(System.currentTimeMillis()); //pega a data atual e preeenche
+	@Id // Tudo isso até idenitty se comporta como chave primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // um valor que será gerado auto incremento
+	private long id; // big int
 
-		
+	@NotNull // Anotacao para dizer que não pode ter titulo nulo
+	@Size(min = 5, max = 100) // anotação para a quantidade de caracter que o cliente consegue mandar como
+								// titulo
+	private String titulo; // tipo o varchar (100)
+
+	@NotNull // Anotacao para dizer que não pode ter titulo nulo
+	// anotação para a quantidade de caracter que o cliente consegue mandar como
+	// titulo
+	@Size(min = 10, max = 500)
+	private String texto;
+
+	@Temporal(TemporalType.TIMESTAMP) // anotação que vai indicar o JPA ibernate que estamos trabalhando com tempo
+	private Date date = new java.sql.Date(System.currentTimeMillis()); // pega a data atual e preeenche
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+	 
 	public long getId() {
 		return id;
 	}
@@ -70,7 +76,13 @@ public class Postagem {
 	public void setDate(Date date) {
 		this.date = date;
 	}
-	
-	
+
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 
 }
